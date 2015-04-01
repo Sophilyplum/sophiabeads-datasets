7a96f6de364b8cb83b5c69a8e9cb149a85678102
Sophia Bethany Coban <sophia.coban@manchester.ac.uk>
2015-04-01 12:38:50 +0100

    updated comments about the setup of mex files.

--- setup.m
+++ setup.m
@@ -1,6 +1,16 @@
 % A short script to build the mex files for forward and back projectors.
 % Saves the output in the mex/ folder.
 %
+%
+% PLEASE NOTE that this script is intended to be a TEMPLATE and should be
+% used if the user is interested in building/testing own projector and
+% backprojector mex files.
+% The script would only need to be run once, and will overwrite the existing
+% projector and backprojector mex files. To avoid this, modify the name after
+% -output option in lines 22 and 23.
+% To run before a SophiaBeads Dataset, uncomment line 21 in sophiabeads.m.
+%
+%
 % Copyright (c) 2015 Sophia Bethany Coban
 % Code is available via the SophiaBeads Datasets project.
 % University of Manchester.
